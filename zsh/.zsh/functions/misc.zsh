47dc94942d3b16841ca7307cbe1fb83a5665198f
Daniel Strittmatter <daniel@stritty.de>
2013-03-12 23:52:30 +0100

    zsh: add imv function

--- zsh/.zsh/functions/misc.zsh
+++ zsh/.zsh/functions/misc.zsh
@@ -274,6 +274,16 @@ send2kindle() {
   ssh smt "echo have fun | mutt -s $subject -a \"/tmp/${file_name}\" -- $mail_addr && rm \"/tmp/${file_name}\""
 }
 
+imv() {
+  local src dst
+  for src; do
+    [[ -e $src ]] || { print -u2 "$src does not exist"; continue }
+    dst=$src
+    vared dst
+    [[ $src != $dst ]] && mkdir -p $dst:h && mv -n $src $dst
+  done
+}
+
 # Less
 LESSOPEN="|/usr/bin/lesspipe.sh %s"
 export LESSOPEN
