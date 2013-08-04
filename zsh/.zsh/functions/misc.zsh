2208d89f328071b95e8ef32189f6069fc8b7f48f
Daniel Strittmatter <daniel@smattr.de>
2013-08-04 13:38:53 +0200

    Remove unnecessary things for servers
    Conflicts:
    	subl/Packages/User/Preferences.sublime-settings
    	zsh/.zsh/00-exports.zsh
    	zsh/.zsh/10-aliases.zsh
    	zsh/.zsh/15-aliases-global.zsh
    	zsh/.zsh/40-bookmarks.zsh
    	zsh/.zsh/functions/audio.zsh
    	zsh/.zsh/functions/backup.zsh
    	zsh/.zsh/functions/cfg.zsh
    	zsh/.zsh/functions/misc.zsh
    	zsh/.zsh/functions/notify.zsh
    	zsh/.zsh/functions/osx.zsh
    	zsh/.zsh/functions/scan.zsh
    	zsh/.zsh/functions/secrets.zsh
    	zsh/.zsh/functions/smServer.zsh
    	zsh/.zsh/functions/streaming.zsh

--- zsh/.zsh/functions/misc.zsh
+++ zsh/.zsh/functions/misc.zsh
@@ -1,18 +1,16 @@
-# Make and change to directory
 function cdmkdir {
   if mkdir $1; then
     cd $1
   fi
 }
 
-# Extract archives
 function extract() {
   if [ -f $1 ]; then
     case $1 in
       *.tar.bz2)        tar xvjf $1     ;;
       *.tar.gz)         tar xzvf $1     ;;
       *.bz2)            bunzip2 $1      ;;
-      *.rar)            unrar x $1      ;;
+      *.rar)            unrar x $1       ;;
       *.gz)             gunzip $1       ;;
       *.tar)            tar xvf $1      ;;
       *.tbz2)           tar xvjf $1     ;;
@@ -27,7 +25,6 @@ function extract() {
   fi
 }
 
-# Change to containing directory if file argument is given
 function cd() {
   [[ -z "$1" ]] && builtin cd && return
   local dest="$1"
@@ -41,39 +38,19 @@ function chpwd() {
   command ls -a
 }
 
-# Download URL in clipboard
-function getit() {
-  aria2c $(pbpaste)
-}
-
 function minimalPrompt() {
   PS1="$ "
   clear
 }
 
-# Fuzzy find in current directory
 function findhere() {
   local what="$1"
   shift
   find . -iname "*${what}*" $*
 }
 
-function scpunihp() {
-  scp $1 uni:../home_page/html-data/$2
-  echo "http://home.in.tum.de/~strittma/${2}/${1}"
-}
-
-# Match line with regex and return the content of the first group
 function sgrep() {
   cat | perl -ne "print \"\$1\\n\" if $1"
-}
-
-function beep() {
-  echo -n "\07"
-}
-
-function google() {
-  chromium "http://www.google.de/search?q=$*"
 }
 
 function pdfmerge() {
@@ -85,16 +62,6 @@ function pdfgrayscale() {
 
 }
 
-# Open argument or current directory
-function x() {
-  if [[ $# -eq 0 ]]; then
-    open . &>/dev/null &|
-  else
-    open $* &>/dev/null &|
-  fi
-}
-
-# Nice df layout
 function df() {
   if [[ -z "$*" ]]; then
     command df -h | column -t
@@ -103,56 +70,10 @@ function df() {
   fi
 }
 
-function matrix() {
-  tr -c "[:digit:]" " " < /dev/urandom | dd cbs=$COLUMNS conv=unblock | GREP_COLOR="1;32" grep --color "[^ ]"
-}
-
-spectrum_ls() {
+function spectrum_ls() {
   for code in {000..255}; do
     print -P -- "$code: %F{$code}Test%f"
   done
-}
-
-function dotView() {
-  if [[ -z $1 ]]; then
-    echo "Usage: $0 dotfile [options]"
-    exit 1
-  fi
-  fn="$1"
-  shift
-  dot -Tpng -o /tmp/${fn:t:r}.png $fn $* && open /tmp/${fn:t:r}.png
-}
-
-function workflowy2otl() {
-	sed -e 's/^\(\s*\)-/\1/' -e 's/  /\t/g' $*
-}
-
-function sshh() {
-  TERM=screen ssh -t $* 'tmux attach || tmux new || screen -DR';
-}
-
-function dashboard() {
-  tmux new-session \; send-keys "cd ~/.hubble && hubble" "C-m" \; split-window -v \; send-keys "while true; do clear; lsof -i -nP | grep -i establish | awk '{print \$1\" \"\$9}'; sleep 10; done" "C-m" \; split-window -h \; send-keys "ssh smserver 'while true; do echo -----------------------------; lsof -i -nP | grep -i listen | sort -u; sleep 10; done'" "C-m"
-}
-
-# Send document to kindle email address
-function send2kindle() {
-  local mail_addr="amazon_10697@kindle.com"
-
-  if [[ -z $1 ]]; then
-    echo "Please provide a file"
-    return 1
-  fi
-
-  file_path="$1"
-  file_name="$(basename $file_path)"
-  local subject="hello"
-  if [[ ${file_name:e} == "pdf" ]] && [[ $2 != "noconvert" ]]; then
-    echo "Using convert option"
-    subject="convert"
-  fi
-  scp $file_path smt:/tmp
-  ssh smt "echo have fun | mutt -s $subject -a \"/tmp/${file_name}\" -- $mail_addr && rm \"/tmp/${file_name}\""
 }
 
 # Interactively rename files
@@ -166,22 +87,10 @@ function imv() {
   done
 }
 
-function sb() {
-  if [[ -z "$1" ]]; then
-    subl3 -n .
-  else
-    subl3 $*
-  fi
-}
-
 function tm() {
   tmux-layout $* 2>/dev/null ||
   tmux-layout $HOME/.tmux-layouts/$1.json 2>/dev/null ||
   tmux $*
-}
-
-function ta() {
-  tmux attach $* || tm $*
 }
 
 function lineSelect() {
