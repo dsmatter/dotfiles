1a93338cf31f3ffa4862700ab4f9f65b7fe348d6
Daniel Strittmatter <daniel@smattr.de>
2014-07-20 21:28:09 +0200

    [zsh] global aliases

--- zsh/.zsh/15-aliases-global.zsh
+++ zsh/.zsh/15-aliases-global.zsh
@@ -1,10 +1,11 @@
 #!/bin/zsh
 
 alias -g A='; alert'
-alias -g C='$(pbpaste)'
+alias -g C=' "$(pbpaste)" '
 alias -g CC='| pbcopy'
 alias -g E=' 2>/dev/null'
 alias -g EE=' 2>&1'
+alias -g F=' "$(fzf)"'
 alias -g G='| egrep -i --color'
 alias -g L='| less'
 alias -g P="| $PAGER"
