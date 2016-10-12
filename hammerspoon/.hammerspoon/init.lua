a7d1fad4d547affbda42b11797d11460184b857c
Daniel Strittmatter <daniel@smattr.de>
2016-10-12 18:11:27 +0200

    [hammerspoon] add config

---
+++ hammerspoon/.hammerspoon/init.lua
@@ -0,0 +1,73 @@
+---
+--- Hyper Key Setup ---
+---
+
+-- A global variable for the Hyper Mode
+k = hs.hotkey.modal.new({}, "F17")
+
+-- Enter Hyper Mode when F18 (Hyper/Capslock) is pressed
+pressedF18 = function()
+  k.triggered = false
+  k:enter()
+end
+
+-- Leave Hyper Mode when F18 (Hyper/Capslock) is pressed,
+--   send ESCAPE if no other keys are pressed.
+releasedF18 = function()
+  k:exit()
+  --if not k.triggered then
+  --  hs.eventtap.keyStroke({}, 'ESCAPE')
+  --end
+end
+
+-- Bind the Hyper key
+f18 = hs.hotkey.bind({}, 'F18', pressedF18, releasedF18)
+
+---
+--- Helper Functions
+---
+
+local function launcher(app)
+  return function()
+    hs.application.launchOrFocus(app)
+  end
+end
+
+local function showOrHide(bundleid)
+  return function()
+    local app = hs.application.applicationsForBundleID(bundleid)[1]
+    if (app:ishidden()) then
+      app:activate()
+    else
+      app:hide()
+    end
+  end
+end
+
+---
+--- Key Bindings
+---
+
+k:bind('', 'C', launcher("Google Chrome"))
+k:bind('', "A", launcher("Atom"))
+k:bind('', "B", launcher("Sublime Text"))
+k:bind('', "C", launcher("Google Chrome"))
+k:bind('', "S", launcher("Safari"))
+k:bind('', "T", launcher("Hyper"))
+--k:bind('', "T", launcher("Terminal"))
+--k:bind('', "T", launcher("iTerm"))
+k:bind('', "E", launcher("STS"))
+k:bind('', "I", launcher("IntelliJ IDEA CE"))
+k:bind('', "F", launcher("Finder"))
+k:bind('', "G", launcher("SourceTree"))
+k:bind('', "H", launcher("GitUp"))
+k:bind('', "M", launcher("Mail Pilot"))
+k:bind('', "N", launcher("Evernote"))
+k:bind('', "P", launcher("Preview"))
+k:bind('', "U", launcher("Spotify"))
+k:bind('', "V", launcher("Emacs"))
+k:bind('', "W", launcher("WebStorm"))
+k:bind('', "X", launcher("Xcode"))
+k:bind('', "Z", launcher("Slack"))
+
+
