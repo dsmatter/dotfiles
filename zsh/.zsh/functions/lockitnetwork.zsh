e03b2d6ecb2aed70276ba3cdb5bf87d9c61794c3
Daniel Strittmatter <daniel@lockitnetwork.com>
2018-03-05 11:24:32 +0100

    [zsh] add LockitNetwork functions

---
+++ zsh/.zsh/functions/lockitnetwork.zsh
@@ -0,0 +1,9 @@
+function lnusers {
+  URL="$LN_API_BASE_URL/projects/$1/users?token=$LN_API_TOKEN"
+  if [[ "$2" != "" ]]; then
+    curl -i -d "user=$2" $URL
+  fi
+
+  echo $URL
+  curl -i "$URL"
+}
