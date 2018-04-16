64f3984c8acc0ed1e9b74cbb795bb14abf7954c5
Daniel Strittmatter <daniel@lockitnetwork.com>
2018-04-16 12:00:18 +0200

    [zsh] add lnroles

--- zsh/.zsh/functions/lockitnetwork.zsh
+++ zsh/.zsh/functions/lockitnetwork.zsh
@@ -8,6 +8,16 @@ function lnusers {
   curl -i "$URL"
 }
 
+function lnroles {
+  URL="$LN_API_BASE_URL/admin/users/$1/roles?token=$LN_API_TOKEN"
+  if [[ "$2" != "" ]]; then
+    curl -i -X PUT -d "role=$2" $URL
+  fi
+
+  echo $URL
+  curl -i "$URL"
+}
+
 function allreports {
   curl -i "https://webtools.lockitnetwork.com/api/reports/all?token=$LN_API_TOKEN"
 }
